f17822b25a38c56550bae7167c01b110c346b018
DevJulio <julioocesarmoura2008@gmail.com>
2025-02-04 08:28:21 -0300

    Delete Projeto-sesi-main/Projeto-sesi-main/Projeto-Sesi-master directory

--- Projeto-sesi-main/Projeto-sesi-main/Projeto-Sesi-master/src/main/java/com/sesi/projeto/dto/PagamentoDTO.java
+++
@@ -1,7 +0,0 @@
-package com.sesi.projeto.dto;
-
-import java.time.Instant;
-
-public record PagamentoDTO(Long id, Instant momento) {
-
-}
--- Projeto-sesi-main/Projeto-sesi-main/Projeto-Sesi-master/src/main/java/com/sesi/projeto/repositories/StatusDoProduto.java
+++
@@ -1,7 +0,0 @@
-package com.sesi.projeto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusDoProduto extends JpaRepository<Long, StatusDoProduto> {
-
-}
